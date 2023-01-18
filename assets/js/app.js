605e2595532d03732f2c360e9cc19f20d139ec6f
verum <rodriguez.ibarra.juan.carlos95@gmail.com>
2023-01-18 12:42:26 -0600

    Se modificó el código para su funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,44 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
+//faltaban los . de las demas clases
 
-function displayUser(username) {
+async function displayUser(username) {
+//faltaba el async Debe de ser una función asincrona.
   $n.textContent = 'cargando...';
+  try{
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  if(!response.ok){
+    throw new Error(response.statusText);
+  }
+
+  const data = await response.json(); //Se creó una constante para 
+  // console.log(data);
+  $n.textContent = `${data.name}`; //Se utilizan backticks
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`
+}catch (err){
+  handleError(err);
+} //se añadió el cath para el error
 }
+
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
+  //Se agregó $
 
-displayUser('stolinski').catch(handleError);
+
+displayUser('stolinski');
+// se retiró el catch error
+
+
+
+
+
+
+
